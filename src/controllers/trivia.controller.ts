41f8c0244c029a5a44ef807005785c6aa1a14fab
SantiRagnar <santbarrios130600@gmail.com>
2023-03-11 16:52:07 -0300

    conectando trivia

--- src/controllers/trivia.controller.ts
+++ src/controllers/trivia.controller.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
+import { triviaResponse } from "../interface";
 
-export async function getTriviaById(id: string){
-  try{
-    const res = await axios.get(``)
-    console.log(id)
-    return res
-  }catch (err){
-    console.error("err")
-    return err
+export async function getTriviaById(id: string) {
+  try {
+    const res: triviaResponse = await axios.get(
+      `${process.env.URL}/api/v1.0/trivia/${id}`
+    );
+    console.log(id);
+    return res;
+  } catch (err) {
+    console.error("err");
+    return err;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,46 @@
-import express from 'express'
-import http from "http"
-import { Server } from "socket.io"
-import morgan from "morgan"
-import cors from "cors"
-import { getTriviaById } from './controllers/trivia.controller'
-require('dotenv').config();
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import morgan from "morgan";
+import cors from "cors";
+import { getTriviaById } from "./controllers/trivia.controller";
 
-const app = express()
+require("dotenv").config();
+
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://127.0.0.1:5173',
-  }
+    origin: "http://127.0.0.1:5173",
+  },
 });
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-io.on('connection', (socket)=>{
-  console.log('user connected')
-  console.log(socket.id)
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket.id);
 
-  socket.on("get-trivia", (data)=>{
-    console.log("get-trivia", data)
-    const res = getTriviaById(data.id)
-    console.log({res})
-    socket.emit("get-trivia-res", { res: res });
-  })
-})
+  socket.on("get-triviaById", async (data) => {
+    console.log({ data });
+    try {
+      const res: any = await getTriviaById(data.id);
+      console.log({ res });
+      res.status
+        ? socket.emit("get-triviaById-res", { res: res })
+        : socket.emit("get-triviaById-res", { err: "Hubo problemas" });
+    } catch (err) {
+      console.log(err);
+      socket.emit("get-triviaById-res", { err: "Hubo problemas" });
+    }
+  });
+});
 
+console.log(process.env.PORT);
 
-console.log(process.env.PORT)
-
-server.listen(process.env.PORT,()=>{
-  console.log('listening on ' + process.env.PORT)
-})
+server.listen(process.env.PORT, () => {
+  console.log("listening on " + process.env.PORT);
+});
