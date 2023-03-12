05bfb6abc36ba0b3f2931490d88514e639d8c98d
julian-salinas <juliansalinasm@gmail.com>
2023-03-12 16:32:35 -0300

    Agrego mocks a APIs entregas y desafios

---
+++ src/mocks/entregas.mock.ts
@@ -0,0 +1,97 @@
+import nock from "nock";
+
+const entregasTriviaModerated = nock("http://0.0.0.0:8095")
+  .get("/api/v1.0/trivia/0")
+  .reply(200, {
+    id: 5,
+    name: "PI-06 Unidad",
+    description: "Trivia multiplechoice, debe seleccionar la respuesta correcta en cada desafio",
+    moderated: true, 
+    end_date: ""
+  })
+
+const entregasTriviaNotModerated = nock("http://0.0.0.0:8095")
+  .get("/api/v1.0/trivia/1")
+  .reply(200, {
+    id: 5,
+    name: "PI-06 Unidad",
+    description: "Trivia multiplechoice, debe seleccionar la respuesta correcta en cada desafio",
+    moderated: false, 
+    end_date: "2023-02-01"
+  })
+
+const entregasChallenges = nock("http://0.0.0.0:8095")
+.get("/api/v1.0/trivia/0/challenges")
+.reply(200, {
+  challenges: [
+    3, 20, 21, 49, 52
+  ]
+})
+  
+const entregasUsersStaff = nock("http://0.0.0.0:8095")
+  .get("/api/v1.0/trivia/0/user/juli")
+  .reply(200, {
+    username: "santi",
+    name: "Santiago Barrios",
+    is_staff: true,
+    role: "estudiante",
+    score: 80,
+    bonus: 40
+  })
+
+const entregasUsersNotStaff = nock("http://0.0.0.0:8095")
+.get("/api/v1.0/trivia/0/user/juli")
+.reply(200, {
+  username: "juli",
+  name: "Julián Salinas",
+  is_staff: false,
+  role: "estudiante",
+  score: 80,
+  bonus: 40
+})
+
+const entregasGetResults = nock("http://0.0.0.0:8095")
+  .get("/api/v1.0/trivia/0/results")
+  .reply(200, 
+    {
+      results: [
+        {
+          username: "santiago_barrios", 
+          score: 80, 
+          bonus: 40
+        },
+        {
+          username: "julian_salinas", 
+          score: 60, 
+          bonus: 30
+        }
+    ]
+  })
+
+const entregasPostResults = nock("http://0.0.0.0:8095")
+  .post("/api/v1.0/trivia/0/results")
+  .reply(200, 
+    {
+      results: [
+        {
+          username: "santiago_barrios", 
+          score: 80, 
+          bonus: 40
+        },
+        {
+          username: "julian_salinas", 
+          score: 60, 
+          bonus: 30
+        }
+    ]
+  })
+
+export default { 
+  entregasTriviaModerated,
+  entregasTriviaNotModerated,
+  entregasChallenges,
+  entregasUsersStaff,
+  entregasUsersNotStaff,
+  entregasGetResults,
+  entregasPostResults
+}
